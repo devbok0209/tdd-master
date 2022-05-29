67c736792259956b833d0909e060279d907f0baf
devbok0209 <devbok0209@bodycodi.com>
2022-05-29 14:53:17 +0900

    [FEAT] 통과하는 테스트 코드 작성

--- src/main/java/com/currency/Dollar.java
+++ src/main/java/com/currency/Dollar.java
@@ -1,4 +1,13 @@
 package com.currency;
 
 public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
 }
