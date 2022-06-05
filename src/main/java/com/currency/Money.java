514fd590676e2f9d70aa5c6bc6850a1d5020249e
devbok0209 <devbok0209@bodycodi.com>
2022-06-06 02:17:39 +0900

    [TEST & FEAT] 선언부를 공통 상위 클래스로 옮김, 불필요해진 하위 클래스의 테스트의 발견, 공용 times()

--- src/main/java/com/currency/Money.java
+++ src/main/java/com/currency/Money.java
@@ -1,16 +1,22 @@
 package com.currency;
 
-public class Money {
+public abstract class Money {
     protected int amount;
-
     static Dollar dollar(int amount) {
         return new Dollar(amount);
+
     }
 
+    static Franc franc(int amount) {
+        return new Franc(amount);
+
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount
                 && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multiplier);
 }
--- src/test/java/com/currency/DollarTest.java
+++ src/test/java/com/currency/DollarTest.java
@@ -11,15 +11,15 @@ class DollarTest {
     @DisplayName("더하기 테스트")
     void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     @DisplayName("equal 테스트")
     void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
     }
 
 }
