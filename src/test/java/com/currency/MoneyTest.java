a17f4425bd296b5610f222317e34c297408d5f59
devbok0209 <devbok0209@bodycodi.com>
2022-06-12 22:42:48 +0900

    [TEST & FEAT] 생성자만 남은 Dollar, Franc 삭제

--- src/test/java/com/currency/MoneyTest.java
+++ src/test/java/com/currency/MoneyTest.java
@@ -12,8 +12,6 @@ class MoneyTest {
     void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(6)));
     }
 
@@ -22,12 +20,5 @@ class MoneyTest {
     void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    @DisplayName("다른 클래스지만 서로 같아야하는 테스트")
-    void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(
-                new Franc(10, "CHF")));
     }
 }
