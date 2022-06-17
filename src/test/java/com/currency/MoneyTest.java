326f980a7d6f3fe01f9cab1f40f9bd03f9c9a995
devbok0209 <devbok0209@bodycodi.com>
2022-06-17 09:33:08 +0900

    [CHAPTER13] bank.reduce(Money) 하는 테스트 코드 추가

--- src/test/java/com/currency/MoneyTest.java
+++ src/test/java/com/currency/MoneyTest.java
@@ -50,4 +50,13 @@ class MoneyTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(7), result);
     }
+
+    @Test
+    @DisplayName("bank.reduce가 Money를 받는 테스트")
+    void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
 }
