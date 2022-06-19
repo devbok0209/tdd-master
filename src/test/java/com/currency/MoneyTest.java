c1a67aa5d52af3fe43618a1e62438112c989c5b7
devbok0209 <devbok0209@bodycodi.com>
2022-06-20 01:31:35 +0900

    [CHAPTER14] 프랑을 달러로 바꾸는 테스트 추가

--- src/test/java/com/currency/MoneyTest.java
+++ src/test/java/com/currency/MoneyTest.java
@@ -59,4 +59,12 @@ class MoneyTest {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    @DisplayName("프랑을 달러로 바꾸는 테스트")
+    void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
