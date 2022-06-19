1a3f6c3de3ffef5500d89a39605c791e6d94a512
devbok0209 <devbok0209@bodycodi.com>
2022-06-20 01:38:41 +0900

    [CHAPTER14] Money가 환율을 알아버린 코드 -> Bank에게 환율을 물어보도록 코드 변경

--- src/main/java/com/currency/Bank.java
+++ src/main/java/com/currency/Bank.java
@@ -1,6 +1,13 @@
 package com.currency;
 
 public class Bank {
+
+    public int rate(String from, String to) {
+        return from.equals("CHF") && to.equals("USD")
+               ? 2
+               : 1;
+    }
+
     public Money reduce(Expression source, String to) {
         return source.reduce(to);
     }
